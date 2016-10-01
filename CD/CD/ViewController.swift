a86b40770069213af0258d75de05f0b7d41517d0
Vladislav Simovic <simke_deluxe@yahoo.co.uk>
2016-10-01 13:08:14 +0200

    Created CustomDeal project

---
+++ CD/CD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CD
+//
+//  Created by Vladislav Simovic on 10/1/16.
+//  Copyright © 2016 CustomDeal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
