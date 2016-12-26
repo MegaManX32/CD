786dbbf23559de02d8bf2fef7d4916dd5ed8521e
Vladislav Simovic <simke_deluxe@yahoo.co.uk>
2016-12-26 23:56:42 +0100

    Offer screen done

--- CD/CD/View Controllers/HostReviewRequestsViewController.swift
+++ CD/CD/View Controllers/HostReviewRequestsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  RiderListPreviewViewController.swift
+//  HostReviewRequestsViewController.swift
 //  CD
 //
 //  Created by Vladislav Simovic on 12/26/16.
@@ -10,11 +10,14 @@ import UIKit
 
 fileprivate let avatarImageViewHeightAndWidth: CGFloat = 80
 fileprivate let interestImageViewHeightAndWidth: CGFloat = 30
+fileprivate let riderListTextViewViewMinimumHeight: CGFloat = 95
+fileprivate let containerViewMinimumHeight: CGFloat = 530
 
-class RiderListPreviewViewController: UIViewController {
+class HostReviewRequestsViewController: UIViewController {
     
     // MARK: - Properties
     
+    @IBOutlet weak var numberOfRequestsLabel : UILabel!
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitelLabel: UILabel!
@@ -22,9 +25,11 @@ class RiderListPreviewViewController: UIViewController {
     @IBOutlet weak var interest1ImageView : UIImageView!
     @IBOutlet weak var interest2ImageView : UIImageView!
     @IBOutlet weak var interest3ImageView : UIImageView!
+    @IBOutlet weak var pageView: UIView!
     
     @IBOutlet weak var bestTimeLabel : UILabel!
     @IBOutlet weak var riderListTextView : UITextView!
+    @IBOutlet weak var containerViewHeightConstraint : NSLayoutConstraint!
     
     // MARK: - View Lifecycle
 
@@ -46,6 +51,11 @@ class RiderListPreviewViewController: UIViewController {
         self.interest3ImageView.layer.cornerRadius = avatarImageViewHeightAndWidth / 2.0
         self.interest3ImageView.layer.borderColor = UIColor.white.cgColor
         self.interest3ImageView.layer.borderWidth = 2.0
+        
+        // update container view height
+        self.view.layoutIfNeeded()
+        let sizeThatFits = self.riderListTextView.sizeThatFits(CGSize(width: self.riderListTextView.frame.width, height: CGFloat.greatestFiniteMagnitude))
+        self.containerViewHeightConstraint.constant = max(containerViewMinimumHeight, containerViewMinimumHeight + sizeThatFits.height - riderListTextViewViewMinimumHeight)
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,5 +67,11 @@ class RiderListPreviewViewController: UIViewController {
     
     @IBAction func backAction(sender: UIButton) {
         _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func ignoreOffer(sender: UIButton) {
+        UIView.transition(from: self.pageView, to: self.pageView, duration: 0.5, options: [.transitionCurlUp, .showHideTransitionViews]) { (finished) in
+            // do nothing
+        }
     }
 }
