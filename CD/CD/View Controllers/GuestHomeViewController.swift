06c0e2185cdb952ad2e90b7c81075ba9af0c8f0b
Vladislav Simovic <simke_deluxe@yahoo.co.uk>
2016-12-26 15:12:51 +0100

    Guest screen finished

--- CD/CD/View Controllers/GuestHomeViewController.swift
+++ CD/CD/View Controllers/GuestHomeViewController.swift
@@ -8,14 +8,54 @@
 
 import UIKit
 
-class GuestHomeViewController: UIViewController {
+fileprivate let someRequestsViewHeight: CGFloat = 122
+
+class GuestHomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var overviewLabel : UILabel!
+    @IBOutlet weak var yourRiderListView : UIView!
+    @IBOutlet weak var riderListOffersView : UIView!
+    @IBOutlet weak var riderListOffersViewHeightConstraint : NSLayoutConstraint!
+    @IBOutlet weak var createRiderListView : UIView!
+    @IBOutlet weak var tableView : UITableView!
+    @IBOutlet weak var numberOfRiderListOffersLabel : UILabel!
+    
+    var yourRiderList : RiderList?
+    var riderListOffersArray = [1, 2, 4, 4 ,5]
     
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if self.riderListOffersArray.count != 0 {
+            self.overviewLabel.isHidden = false
+            self.yourRiderListView.isHidden = false
+            self.riderListOffersView.isHidden = false
+            self.createRiderListView.isHidden = true
+            
+            // prepare views for rider list and offers
+            self.yourRiderListView.layer.cornerRadius = 4.0
+            self.yourRiderListView.layer.borderColor = UIColor.white.cgColor
+            self.yourRiderListView.layer.borderWidth = 2.0
+            
+            self.riderListOffersView.layer.cornerRadius = 4.0
+            self.riderListOffersView.layer.borderColor = UIColor.white.cgColor
+            self.riderListOffersView.layer.borderWidth = 2.0
+            self.riderListOffersViewHeightConstraint.constant = CGFloat(self.riderListOffersArray.count) * GuestMenuTableViewCell.cellHeight() + someRequestsViewHeight
+            
+            let endString = (self.riderListOffersArray.count == 1) ? "!" : "s!"
+            self.numberOfRiderListOffersLabel.text = "You have \(self.riderListOffersArray.count) " + "offers" + endString
+        }
+        else {
+            self.overviewLabel.isHidden = true
+            self.yourRiderListView.isHidden = true
+            self.riderListOffersView.isHidden = true
+            self.createRiderListView.isHidden = false
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,5 +72,29 @@ class GuestHomeViewController: UIViewController {
     @IBAction func createNewRiderList(sender: UIButton) {
         let controller = self.storyboard?.instantiateViewController(withIdentifier: "CreateRiderListViewController") as! CreateRiderListViewController
         self.show(controller, sender: self)
+    }
+    
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.riderListOffersArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:GuestMenuTableViewCell.cellIdentifier(), for: indexPath) as! GuestMenuTableViewCell
+        cell.populateCellWithRaiderListOffer(riderListOffer: nil, isLastCell: indexPath.row == self.riderListOffersArray.count - 1)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return GuestMenuTableViewCell.cellHeight()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
---
+++ CD/CD/View Controllers/GuestMenuTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  GuestMenuTableViewCell.swift
+//  CD
+//
+//  Created by Vladislav Simovic on 12/26/16.
+//  Copyright © 2016 CustomDeal. All rights reserved.
+//
+
+import UIKit
+
+class GuestMenuTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    @IBOutlet weak var avatarImageView : UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var countryLabel : UILabel!
+    @IBOutlet weak var descriptionLabel : UILabel!
+    @IBOutlet weak var separatorView : UIView!
+    
+    @IBOutlet weak var star1ImageView : UIImageView!
+    @IBOutlet weak var star2ImageView : UIImageView!
+    @IBOutlet weak var star3ImageView : UIImageView!
+    @IBOutlet weak var star4ImageView : UIImageView!
+    @IBOutlet weak var star5ImageView : UIImageView!
+    
+    // MARK: - Cell lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // prepare avatar
+        self.avatarImageView.layer.cornerRadius = 43 / 2.0
+        self.avatarImageView.layer.borderWidth = 2.0;
+        self.avatarImageView.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    // MARK: - Helper methods
+    
+    func populateCellWithRaiderListOffer(riderListOffer:RiderListOffer?, isLastCell: Bool) {
+        self.nameLabel.text = "Vlado"
+        self.countryLabel.text = "Serbia"
+        self.avatarImageView.image = nil
+        self.separatorView.isHidden = isLastCell
+    }
+    
+    // MARK: - Type methods
+    
+    class func cellIdentifier() -> String {
+        return String(describing:self)
+    }
+    
+    class func cellHeight() -> CGFloat {
+        return 110
+    }
+}
