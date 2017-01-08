bac3bc7cf79e474ea0cec5ba093be52f12934e54
Vladislav Simovic <simke_deluxe@yahoo.co.uk>
2017-01-08 13:23:39 +0100

    Provide service screen

---
+++ CD/CD/View Controllers/HostProvideServiceViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HostProvideServiceViewController.swift
+//  CD
+//
+//  Created by Vladislav Simovic on 1/8/17.
+//  Copyright © 2017 CustomDeal. All rights reserved.
+//
+
+import UIKit
+
+class HostProvideServiceViewController: UIViewController {
+    
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - User Actions
+    
+    @IBAction func backAction(sender: UIButton) {
+        _ = self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func transportationAction(sender: UIButton) {
+        _ = self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func accomodationAction(sender: UIButton) {
+        _ = self.navigationController?.popToRootViewController(animated: true)
+    }
+
+}
